2c87b313add9cd677ec7a9c3e56214925c0b0bb0
Luiz Camargo <luizhp.camargo@gmail.com>
2020-01-05 21:27:12 -0300

    task: company model, specs and relations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,11 @@
 class Company < ApplicationRecord
+  has_many :employees
+  validates_uniqueness_of :document
+  validate :document_valid?
+
+  private
+
+  def document_valid?
+    errors.add(:document, 'is invalid') unless CNPJ.valid?(document)
+  end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'relations' do
+    it { is_expected.to have_many :employees }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_uniqueness_of :document }
+
+    context '#document' do
+      context 'when cpf is invalid' do
+        subject { described_class.new(document: '12345678901200') }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'when cpf is valid' do
+        subject { described_class.new(document: CNPJ.generate) }
+
+        it { is_expected.to be_valid }
+      end
+    end
+  end
 end
