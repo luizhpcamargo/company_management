823ea4c0d03bb9e39529ee88dca232c9272326ea
Luiz Camargo <luizhp.camargo@gmail.com>
2020-01-05 21:25:57 -0300

    task: employes validations, relations and tests

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+  belongs_to :company
+  validates_uniqueness_of :document
+  validate :document_valid?
+
+  private
+
+  def document_valid?
+    errors.add(:document, 'is not valid') unless CPF.valid?(document)
+  end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'relations' do
+    it { is_expected.to belong_to :company }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_uniqueness_of :document }
+
+    context '#document' do
+      context 'when cpf is invalid' do
+        subject { described_class.new(document: '12345678900', company: Company.new) }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'when cpf is valid' do
+        subject { described_class.new(document: CPF.generate, company: Company.new) }
+
+        it { is_expected.to be_valid }
+      end
+    end
+  end
 end
