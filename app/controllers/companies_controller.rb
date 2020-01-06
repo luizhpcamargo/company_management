ce692ef5b583a9ad531d548a8ef53d2c60a2bf49
Luiz Camargo <luizhp.camargo@gmail.com>
2020-01-06 17:44:41 -0300

    tasks: companies simple crud

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :load_company, only: %i[show destroy update]
+
   def create
     company = Company.new(company_params)
 
@@ -9,7 +11,41 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def show
+    if @company.present?
+      render json: @company.to_json, status: 200
+    else
+      render json: { errors: ['Company not found'] }.to_json, status: 404
+    end
+  end
+
+  def index
+    companies = Company.all
+
+    render json: companies.to_json, status: 200
+  end
+
+  def destroy
+    if @company.present? && @company.destroy
+      render json: { message: 'Company destroyed' }, status: 200
+    else
+      render json: { errors: ['Company not found'] }.to_json, status: 404
+    end
+  end
+
+  def update
+    if @company.update(company_params)
+      render json: @company.to_json, status: 200
+    else
+      render json: { errors: @company.errors.full_messages}.to_json, status: 304
+    end
+  end
+
   private
+
+  def load_company
+    @company = Company.find_by(id: params[:id])
+  end
 
   def company_params
     params.require(:company).permit(:name, :company_name, :document)
--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
-  let(:params) do
+  let(:company_params) do
     {
-      company:
-      {
-        name: 'Razão',
-        company_name: 'Fantasia',
-        document: CNPJ.generate
-      }
+      name: 'Razão',
+      company_name: 'Fantasia',
+      document: CNPJ.generate
     }
   end
+
+  let(:params) do
+    {
+      company: company_params
+    }
+  end
+
+  let(:body) { JSON.parse(response.body) }
 
   context '#create' do
     let(:creation) { post :create, params: params }
@@ -25,7 +30,6 @@ RSpec.describe CompaniesController, type: :controller do
       it { expect(response.code).to eq '201' }
 
       context 'calling two times' do
-        let(:body) { JSON.parse(response.body) }
         before { post :create, params: params }
 
         it { expect(response).to_not be_ok }
@@ -33,5 +37,74 @@ RSpec.describe CompaniesController, type: :controller do
         it { expect(response.code).to eq '304' }
       end
     end
+  end
+
+  context '#show' do
+    let!(:company) { Company.create(company_params) }
+
+    before { get :show, params: {id: company.id} }
+
+    it { expect(response).to be_ok }
+    it { expect(body['id']).to eq company.id }
+    it { expect(response.code).to eq '200' }
+
+    context 'not found' do
+      before { get :show, params: { id: 'a' } }
+
+      it { expect(response).to_not be_ok }
+      it { expect(body['errors']).to eq ['Company not found']}
+      it { expect(response.code).to eq '404' }
+    end
+  end
+
+  context '#index' do
+    before { get :index }
+
+    it { expect(body.size).to eq 0 }
+    it { expect(body).to be_a Array }
+    it { expect(response).to be_ok }
+    it { expect(response.code).to eq '200' }
+
+    context 'with content' do
+      before do
+        4.times { Company.create(document: CNPJ.generate) }
+        get :index
+      end
+
+      it { expect(body.size).to eq 4 }
+      it { expect(body).to be_a Array }
+      it { expect(response).to be_ok }
+      it { expect(response.code).to eq '200' }
+    end
+  end
+
+  context '#destroy' do
+    let!(:company) { Company.create(company_params) }
+
+    it { expect{ put :destroy, params: { id: company.id } }.to change{ Company.count}.by(-1) }
+
+    context 'messages and status' do
+      before { put :destroy, params: {id: company.id} }
+
+      it { expect(response).to be_ok }
+      it { expect(body['message']).to eq 'Company destroyed' }
+      it { expect(response.code).to eq '200' }
+    end
+
+    context 'not found' do
+      before { put :destroy, params: { id: 'a' } }
+
+      it { expect(response).to_not be_ok }
+      it { expect(body['errors']).to eq ['Company not found']}
+      it { expect(response.code).to eq '404' }
+    end
+  end
+
+  context '#update' do
+    let!(:company) { Company.create(company_params) }
+    let(:update) { put :update, params: {id: company.id, company: { name: 'Razão 2' }} }
+
+    it { expect { update }.to_not change{ Company.count } }
+    it { expect { update }.to change { company.reload.name }.from('Razão').to('Razão 2') }
   end
 end
